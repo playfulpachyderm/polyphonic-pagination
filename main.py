1dc2cffa976b24f4e2dcaad912a323e7c5c3f268
deinactivation <me@me.com>
2019-07-28 17:14:29 -0400

    working commit

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ import numpy as np
 f_s, y = scipy_load_wav("pi_mono.wav")
 
 for i in np.arange(0,len(y), f_s//10):
+
+    print("%d:%d"%(i,i+f_s/10))
+    sample = y[int(i):int(i+f_s/10)].copy()
+    print(len(sample))
+    sample = sample - np.mean(sample)
+    print(sample)
     print(single_note_from(FFT(y[int(i):int(i+f_s/10)])))
