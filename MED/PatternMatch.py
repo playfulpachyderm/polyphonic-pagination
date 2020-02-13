d7c4745e5fae4f4d30f4c7661d6de7cbba556634
msmith <msmith@neuralink.com>
2020-02-12 22:53:30 -0800

    Added image plotting test of MED algorithm

--- MED/PatternMatch.py
+++ MED/PatternMatch.py
@@ -1,27 +1,45 @@
 import numpy as np
 import collections
+from matplotlib import pyplot as plt
+
+first_row_bias = 0.1
+gap = 1
+penalty = 2
 
 
 # TODO: Swap out the lists for static arrays.
 # It will improve efficiency and run time
 class MEDBuffer:
     def __init__(self, page_buffer, note_buffer_length=0):
+        first_row_bias
         self.page_buffer = page_buffer
-        # self.recent_note_buffer = collections.deque(maxlen=note_buffer_length)
         self.recent_note_buffer = []
-        self.MED_buffer = [[i for i in range(0, len(page_buffer)+1)]]
+
+        # Original method - initialize first row sequentially
+        # self.MED_buffer = [[i for i in range(0, len(page_buffer)+1)]]
+
+        #initialize first row to zeros to avoid biasing to start of page
+        # self.MED_buffer = [[0 for i in range(0, len(page_buffer)+1)]]
+
+        #use global gap value to init first row
+        self.MED_buffer = [[i*first_row_bias for i in range(0, len(page_buffer)+1)]]
+
+
         # self.MED_head = 0
         # self.MED_tail = 0
 
     def note_penalty_lookup(self, played_note, page_note):
         misses = 0
+        # one octave error should have low penalty
 
         for subnote in played_note:
             if subnote not in page_note:
-                misses += 1
+                misses += penalty
         return misses
 
+
     def add_note(self, note):
+        gap
         self.recent_note_buffer.append(note)
         self.MED_buffer.append([0 for i in range(0, len(self.page_buffer)+1)])
         self.update_med_table()
@@ -32,7 +50,6 @@ class MEDBuffer:
 
         for i in range(1, len(self.page_buffer) + 1):
             # Gap/deletion penalty of 1
-            gap = 1
             value1 = self.MED_buffer[-2][i] + gap
             value2 = self.MED_buffer[-1][i-1] + gap
             value3 = self.MED_buffer[-2][i-1]
@@ -50,3 +67,11 @@ class MEDBuffer:
 
     def purge_buffer(self):
         self.MED_buffer = [[i for i in range(1, len(self.page_buffer)+1)]]
+
+    def view_table(self):
+        plt.ioff()
+        plt.imshow(self.MED_buffer, origin="lower")
+        plt.xlabel("Given Score Count")
+        plt.ylabel("Interpreted Note Count")
+        plt.show()
+
---
+++ MED/test_plainPython.py
@@ -0,0 +1,74 @@
+import PatternMatch
+import random
+from matplotlib import pyplot as plt
+
+def cold_start():
+
+    score = [[random.randint(0,30)] for i in range(60)]
+
+    jump = score[int(len(score)/2):]
+    # jump = score[5:]
+
+    obj = PatternMatch.MEDBuffer(score)
+
+    calls = [obj.add_note(i) for i in jump]
+    print(calls)
+    obj.view_table()
+    plt.figure()
+    plt.plot(calls, ".k")
+    plt.xlabel("Interpreted Note Sequence Number")
+    plt.ylabel("Matched Score Note Sequence Number")
+    plt.show()
+
+
+def missed_notes():
+    score = [[random.randint(0,30)] for i in range(30)]
+
+    jump = score.copy()
+
+    [jump.pop(5) for i in range(5)]
+
+    print(jump)
+    print(score)
+
+    obj = PatternMatch.MEDBuffer(score)
+
+    calls = [obj.add_note(i) for i in jump]
+
+    obj.view_table()
+    print(calls)
+    plt.figure()
+    plt.plot(calls, ".k")
+    plt.xlabel("Interpreted Note Sequence Number")
+    plt.ylabel("Matched Score Note Sequence Number")
+    plt.show()
+
+
+def extra_notes():
+    score = [[random.randint(0, 30)] for i in range(30)]
+
+    jump = score.copy()
+
+    [jump.insert(5,[random.randint(0,30)]) for i in range(5)]
+
+    print(jump)
+    print(score)
+
+    obj = PatternMatch.MEDBuffer(score)
+
+    calls = [obj.add_note(i) for i in jump]
+
+    obj.view_table()
+    print(calls)
+    plt.figure()
+    plt.plot(calls, ".k")
+    plt.xlabel("Interpreted Note Sequence Number")
+    plt.ylabel("Matched Score Note Sequence Number")
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    cold_start()
+    missed_notes()
+    extra_notes()
